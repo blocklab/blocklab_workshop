3b552a88bf44c9ac1270f484a12c8b8649258742
Axel Hodler <derxor@gmail.com>
2016-11-01 17:55:53 +0100

    use contract creator as oracle

--- contracts/Presidency.sol
+++ contracts/Presidency.sol
@@ -4,6 +4,11 @@ contract Presidency {
 
   address public trumpBet;
   address public hillaryBet;
+  address public oracle;
+
+  function Presidency() {
+    oracle = tx.origin;
+  }
 
   function bet(uint pick) payable {
     if (msg.value == 10000000000000000000) {
--- test/presidency.js
+++ test/presidency.js
@@ -1,7 +1,8 @@
 contract('Presidency', function(accounts) {
 
-  var alice = accounts[1];
-  var bob = accounts[2];
+  var oscarTheOracle = accounts[0];
+  var aliceForTrump = accounts[1];
+  var bobForHillary = accounts[2];
   var contract;
 
   var TRUMP_PICK = 1;
@@ -13,33 +14,39 @@ contract('Presidency', function(accounts) {
 
   it('accepts bets on trump', function() {
     return contract.bet(TRUMP_PICK, {
-      from: alice,
+      from: aliceForTrump,
       value: web3.toWei(10, 'ether')
     });
   });
 
   it('accepts bets on hillary', function() {
     return contract.bet(HILLARY_PICK, {
-      from: bob,
+      from: bobForHillary,
       value: web3.toWei(10, 'ether')
     }).then(function() {
       contract.hillaryBet.call().then(function(better) {
-        assert.equal(better, bob);
+        assert.equal(better, bobForHillary);
       })
     });
   });
 
   it('knows who bet on trump', function() {
     return contract.trumpBet.call().then(function(better) {
-      assert.equal(better, alice);
+      assert.equal(better, aliceForTrump);
     })
   });
 
   it('does not accept a bet under 10 ether', function() {
     return contract.bet(TRUMP_PICK, {
-      from: bob,
+      from: bobForHillary,
       value: web3.toWei(9, 'ether')
     }).catch(function() {
+    });
+  });
+
+  it('sets the contract creator as an oracle', function() {
+    return contract.oracle.call().then(function(oracle) {
+      assert.equal(oracle, oscarTheOracle);
     });
   });
 });
