1eca75e7cc501cf5e508da39b2938c16e21ef4ab
Axel Hodler <derxor@gmail.com>
2016-11-01 17:26:11 +0100

    accepts bets on trump

--- contracts/Presidency.sol
+++ contracts/Presidency.sol
@@ -1,4 +1,9 @@
 pragma solidity ^0.4.2;
 
 contract Presidency {
+
+  address public trumpBet;
+  function betOnTrump() payable {
+    trumpBet = msg.sender;
+  }
 }
--- test/presidency.js
+++ test/presidency.js
@@ -1,7 +1,22 @@
 contract('Presidency', function(accounts) {
-  it('is defined', function() {
-    var contract = Presidency.deployed();
 
-    assert.isDefined(contract);
+  var alice = accounts[1];
+  var contract;
+
+  beforeEach(function() {
+    contract = Presidency.deployed();
+  });
+
+  it('accepts bets on trump', function() {
+    return contract.betOnTrump.sendTransaction({
+      from: alice,
+      value: web3.toWei(10, 'ether')
+    });
+  });
+
+  it('knows who bet on trump', function() {
+    return contract.trumpBet.call().then(function(better) {
+      assert.equal(better, alice);
+    })
   });
 });
